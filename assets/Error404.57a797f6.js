f4d9f5a806b4f134d85393e428f3ec5b46caff24
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-07-05 01:22:15 +0700

    feat: :sparkles: add googleads :v

---
+++ assets/Error404.57a797f6.js
@@ -0,0 +1,1 @@
+import{o as t,c as o,a as e}from"./vendor.b91d414b.js";const n={},d=e("dt",null,[e("h1",{class:"font-bold text-9xl"}," 404 ")],-1),l=e("dd",null,"Uh oh! Your wanted page doesn't exists, maybe typo?",-1);n.render=function(e,n){return t(),o("div",null,[d,l])};export default n;
