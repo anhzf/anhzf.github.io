e3d09818b055287d9140b4a841116af22d4193c3
Anhzf <me@anhzf.dev>
2024-06-30 19:51:41 +0700

    feat: :sparkles: Layout the `[project]` page

--- web/pages/projects/[project].vue
+++ web/pages/projects/[project].vue
@@ -1,11 +1,27 @@
 <script lang="ts" setup>
 const route = useRoute();
 const { data: doc } = await useAsyncData('project', () => queryContent(route.path).findOne());
+if (!doc.value) throw createError({ statusCode: 404 });
 </script>
 
 <template>
-  <article class="max-w-prose">
-    <h1>{{ doc.title }}</h1>
-    <ContentRenderer :value="doc" />
+  <article class="max-w-screen-xl mx-auto flex flex-col gap-8 px-2 py-8">
+    <h1 class="text-3xl lg:text-5xl font-semibold">{{ doc!.title }}</h1>
+
+    <p class="text-lg font-serif font-normal text-slate">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque commodi molestiae natus excepturi iure
+      dignissimos asperiores quia veniam eius perspiciatis.
+    </p>
+
+    <img :src="doc!.image ?? doc!.thumbnail" :alt="doc!.title" :width="800"
+      class="w-full h-50vh object-cover rounded" />
+
+    <ContentRenderer :value="doc!" class="prose lg:text-lg">
+      <template #empty>
+        <i class="">
+          No details content provided.
+        </i>
+      </template>
+    </ContentRenderer>
   </article>
 </template>
--- web/uno.config.ts
+++ web/uno.config.ts
@@ -1,4 +1,6 @@
-import { defineConfig, presetIcons, presetTypography, presetUno, presetWebFonts, transformerDirectives, transformerVariantGroup } from 'unocss';
+import {
+  defineConfig, presetIcons, presetTypography, presetUno, presetWebFonts, transformerDirectives, transformerVariantGroup,
+} from 'unocss';
 
 export default defineConfig({
   presets: [
@@ -8,6 +10,7 @@ export default defineConfig({
     presetWebFonts({
       fonts: {
         sans: 'Inter:400,500,600,700,800,900',
+        serif: 'Cabin:400'
       },
     }),
   ],
