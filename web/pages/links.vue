b270335ca61e4017a36e904879c3a7c1988d52a8
Anhzf <me@anhzf.dev>
2024-07-12 14:13:24 +0700

    feat: :sparkles: Update links content and layout

--- web/pages/links.vue
+++ web/pages/links.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="pb-20 flex flex-col justify-center items-center">
+  <div class="pb-20 flex flex-col justify-center">
     <BigHero />
 
     <main id="main" data-scroll-section class="flex flex-col">
       <div data-scroll data-scroll-speed="6" data-scroll-delay="0.03" data-scroll-offset="200"
-        class="group max-w-screen-sm px-4 sm:px-6 py-10 bg-white flex flex-col justify-center items-stretch gap-y-10 shadow-md">
+        class="group self-center w-full max-w-screen-sm px-4 sm:px-6 py-10 bg-white flex flex-col justify-center items-stretch gap-y-10 shadow-md">
         <div class="w-full px-4 pb-6 border-b border-indigo-100">
           <h2
             class="font-bold bg-clip-text text-transparent bg-gradient-to-br from-indigo-500 to-light-blue-500 uppercase">
