a3fd77e5491bcde9bc2eddc361c71875fc497ee5
Anhzf <me@anhzf.dev>
2024-07-12 20:53:20 +0700

    feat: :sparkles: Change the jargon

--- web/components/BigHero.vue
+++ web/components/BigHero.vue
@@ -1,15 +1,3 @@
-<script lang="ts" setup>
-const whatBuild = Object.freeze(['Web App', 'Mobile App']);
-const whatBuildActiveIndex = ref(0);
-
-onMounted(() => {
-  setInterval(() => {
-    whatBuildActiveIndex.value = (whatBuildActiveIndex.value + 1) < whatBuild.length
-      ? whatBuildActiveIndex.value + 1 : 0;
-  }, 5000);
-});
-</script>
-
 <template>
   <header data-scroll-section class="w-full h-[85vh] p-4 flex flex-col justify-center items-center gap-3">
     <div class="flex flex-col items-center gap-3">
@@ -20,18 +8,8 @@ onMounted(() => {
         </NuxtLink>
       </h1>
       <div data-scroll data-scroll-speed="3" data-scroll-delay="0.015"
-        class="flex items-center flex-no-wrap bg-light-blue-400/50 font-medium text-xl text-center text-white overflow-hidden">
-        <span class="shrink-0 pl-2">Transform your idea into a</span>
-        <TransitionGroup tag="ul" enter-from-class="transition duration-700 transform opacity-30 translate-y-6"
-          enter-to-class="transition duration-700 transform opacity-100 translate-y-0"
-          leave-from-class="transition duration-700 transform opacity-100 translate-y-0"
-          leave-to-class="transition duration-700 transform opacity-30 -translate-y-6"
-          class="relative w-10ch h-full inline-block overflow-hidden mx-1">
-          <li v-for="(el, i) in whatBuild" v-show="whatBuildActiveIndex === i" :key="el"
-            class="absolute inset-0 font-bold inline">
-            {{ el }}
-          </li>
-        </TransitionGroup>
+        class="flex items-center flex-no-wrap bg-light-blue-400/50 font-medium text-xl text-center text-white overflow-hidden px-2">
+        Develop your ideas into a business-valuable app
       </div>
     </div>
   </header>
