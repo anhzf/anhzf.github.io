ce4449aa3df686b49c77fb2a9c9d25c628393af5
Anhzf <me@anhzf.dev>
2024-07-13 15:01:58 +0700

    feat: :sparkles: Improves BigHero jargon layout

--- web/components/BigHero.vue
+++ web/components/BigHero.vue
@@ -1,15 +1,17 @@
 <template>
   <header data-scroll-section class="w-full h-[85vh] p-4 flex flex-col justify-center items-center gap-3">
-    <div class="flex flex-col items-center gap-3">
+    <div class="flex flex-col items-center gap-2">
       <h1 data-scroll data-scroll-speed="2" data-scroll-delay="0.01"
         class="font-black text-6xl text-center text-slate-900">
         <NuxtLink :to="{ name: 'index' }">
           anhzf.dev
         </NuxtLink>
       </h1>
-      <div data-scroll data-scroll-speed="3" data-scroll-delay="0.015"
-        class="flex items-center flex-no-wrap bg-light-blue-400/50 font-medium text-xl text-center text-white overflow-hidden px-2">
-        Develop your ideas into a business-valuable app
+      <div data-scroll data-scroll-speed="3" data-scroll-delay="0.25"
+        class="font-medium text-xl text-center text-white max-w-36ch text-balance">
+        <span class="bg-light-blue-400/50 px-2 py-1 leading-normal box-decoration-clone">
+          Develop your ideas into a business-valuable app
+        </span>
       </div>
     </div>
   </header>
